df6f6cc6b29f05ad285f1742036965e0d16cfb8c
KingTusk <70224576+KingTusk@users.noreply.github.com>
2020-10-20 17:14:27 +0200

    Completed login task

--- Mock-Exam/Mock-Exam.cpp
+++ Mock-Exam/Mock-Exam.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <conio.h>
+#include <Windows.h>
 #include "login.h"
 
 int main()
 {
-    Enterpassword();
+	do {
+		if (fails >= chances) {
+			endlogin = true;
+			std::cout << "\nYou have entered the wrong password 3 or more times, terminating program";
+			return 0;
+		}
+		else {
+			if (Enterpassword()) {
+				endlogin = true;
+				std::cout << "\nYou have entered the correct pasword, menu will appear soon";
+				Sleep(5000);
+			}
+			else if(fails < chances) {
+				std::cout << "\nYou have entered the wrong password, try again, you have " << chances-fails <<" more tries";
+				Sleep(5000);
+			}
+		}
+	} while (!endlogin);
+	std::cout << "Made it to line 26";
 }
--- Mock-Exam/login.h
+++ Mock-Exam/login.h
@@ -1,10 +1,21 @@
 #pragma once
 #include <iostream>
 #include <conio.h>
+#include <windows.h>
+
+//This header file includes all or most of the code used in the login page task
 
 char loginarray[9]{ '1','2','3','4','5','6','7','8','9' };
-char password[5]{ '1','4','5','2','3' };
+char password[4]{ '4','5','2','3' };
 int Aposition = 0;
+int fails = 0;
+bool endlogin = false;
+char input[4]{ 'n','n','n','n' };
+int chances = 3;
+
+void fixAposition();
+void Printtable();
+bool Enterpassword();
 
 void fixAposition() {//this function changes Aposition if it is outside the range of login array
 	if (Aposition > 8) {
@@ -16,9 +27,14 @@ void fixAposition() {//this function changes Aposition if it is outside the rang
 }
 
 void Printtable(){
-	loginarray[Aposition] = '*';
 	for (int i = 0; i < sizeof(loginarray) / sizeof(loginarray[0]); i++) {
-		std::cout << "|" <<loginarray[i]<< "|";
+		if (i == Aposition) {
+			std::cout << "|" << "*" << "|";
+		}
+		else
+		{
+					std::cout << "|" <<loginarray[i]<< "|";
+		}
 		if ((i+1) % 3 == 0) {
 			std::cout << "\n";
 		}
@@ -27,43 +43,63 @@ void Printtable(){
 }
 
 bool Enterpassword() {
-	char input[5]{ 'n','n','n','n','n' };
 	char keypressed = 'n';
-	int nextpos = 0;
+	bool correctpassword = true;
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 4; i++) {
 		input[i] = loginarray[Aposition];
-		Printtable();
+		if (i == 0) {
+			system("cls");
+			Printtable();
+		}
 		keypressed = _getch();
+		if (keypressed == -32 || keypressed == 224) {
+			keypressed = _getch();
+		}
 		switch (keypressed) {
 		case 'w':
 		case'W':
-		case'ÓH':
+		case 72:
 			Aposition -= 3;
 			fixAposition();
+			input[i] = loginarray[Aposition];
 			break;
 		case's':
 		case'S':
-		case'ÓP':
+		case 80:
 			Aposition += 3;
 			fixAposition();
+			input[i] = loginarray[Aposition];
 			break;
 		case 'a':
 		case 'A':
-		case'ÓK':
+		case 75:
 			Aposition--;
 			fixAposition();
+			input[i] = loginarray[Aposition];
 			break;
 		case'd':
 		case'D':
-		case'ÓM':
+		case 77:
 			Aposition++;
 			fixAposition();
+			input[i] = loginarray[Aposition];
 			break;
 		default:
-			std::cout << "You can only use w,a,s,d or the arrow keys!";
+			std::cout << "\nYou can only use w,a,s,d or the arrow keys!";
+			i--;
+			Sleep(3000);
+		}
+		system("cls");
+		Printtable();
+	}
+	Aposition = 0;
+	for (int i = 0; i < sizeof(password) / sizeof(password[0]); i++) {
+		if (password[i] != input[i]) {
+			fails++;
+			correctpassword = false;
+			break;
 		}
 	}
-	return true;
-
+	return correctpassword;
 }
